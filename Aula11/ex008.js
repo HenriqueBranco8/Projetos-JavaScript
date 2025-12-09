586f5a22ed90e9662951809a59bb16375ce9887b
Henrique Branco <henriquebrancodasilvadias@yahoo.com>
2025-12-09 18:54:25 -0300

    aprendendo condições

---
+++ Aula11/ex008.js
@@ -0,0 +1,7 @@
+var velocidade = 15
+console.log(`A velocidade do seu carro é ${velocidade} km/h`)
+if(velocidade > 60){
+    console.log(`Você foi multado! Você passou da velocidade permitida de 60 km/h e sua velocidade foi de ${velocidade} km/h`)
+} else{
+    console.log(`Dirija sempre de cinto de segurança`)
+}
