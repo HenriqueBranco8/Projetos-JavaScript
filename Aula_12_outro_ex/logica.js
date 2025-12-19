b248db0046f38f247404b3132b78aa5e670ab2da
Henrique Branco <henriquebrancodasilvadias@yahoo.com>
2025-12-19 18:50:22 -0300

    Colocando as imagens

--- Aula_12_outro_ex/logica.js
+++ Aula_12_outro_ex/logica.js
@@ -12,23 +12,35 @@ function verificar(){
     if (formularioano.value.length == 0 || formularioano.value > anoatual){
         alert('ERRO! Verifique os dados novamente')
 } else {
+    //pega a opção selecionada pelo usuário (Maculino ou feminino)
     var fsex = document.getElementsByName('radsex')
+
+    //Diminui o ano atual com o ano colocado
     var idade = anoatual - Number(formularioano.value)
+
+    //Vai escrever o genero da pessoa no HTML
     var genero = ''
+    
+    //Cria uma imagem pelo JS (JavaScript)
     var img = document.createElement('img')
     img.setAttribute('id', 'foto')
+
+    //Se a opção selecionada for masculina vai executar esse bloco
     if(fsex[0].checked){
         genero = 'Maculino'
         if(idade >=0 && idade < 12){
             //criança
+            img.setAttribute('src', 'imgs/homem-crianca.jpg')
         } else if (idade >= 12 && idade < 18){
              //adolecente
+            
         }else if (idade >= 18 && idade < 64){
             //adulto
         } else {
             //idoso
         }
 
+    //Se a opção selecionada for feminina vai executar esse bloco
     } else if(fsex[1].checked){
         genero = 'Feminino'
         if(idade >=0 && idade < 12){
@@ -41,6 +53,9 @@ function verificar(){
             //idoso
         }
     }
-    res.innerHTML = `Você tem ${idade} ano e é do gênero ${genero}`
+
+    //Escreve o resultado de tudo no HTML
+    res.innerHTML = `Você tem ${idade} anos e é do gênero ${genero}`
+    res.appendChild(img)
 }
 }
